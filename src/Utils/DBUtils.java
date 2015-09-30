20d278fe6ece0f3cf80c80341086ac387763ae2f
hbless <969012806@qq.com>
2015-09-30 09:31:06 +0800

    final

---
+++ src/Utils/DBUtils.java
@@ -0,0 +1,54 @@
+package Utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBUtils {
+	public static Connection getConnection(){
+		String url="jdbc:sqlserver://localhost:1433;DatabaseName=excel";
+		String username="sa";
+		String password="086539";
+		Connection con=null;
+		try {
+			con=DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+	public static void close(Connection con){
+		if(con!=null){
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void close(PreparedStatement ps){
+		if(ps!=null){
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void close(ResultSet rs){
+		if(rs!=null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
